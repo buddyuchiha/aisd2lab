54a98ebbd55abf9c543beda6bf522dca7edd960a
afrodetka <Pro100biv@mail.ru>
2023-12-19 20:32:34 +0400

    создание конструктора, деструктора, push_tail, pop_head, pop_tail, нахождения индекса

--- mainlist/list.h
+++ mainlist/list.h
@@ -9,6 +9,9 @@ class List
 public:
 	List();
 	~List();
+
+	void push_back(T data);
+
 private:
 	
 	template <typename T>
@@ -39,4 +42,26 @@ List<T>::List()
 template <typename T>
 List<T>::~List()
 {
+}
+
+template<typename T>
+inline void List<T>::push_back(T data)
+{
+	if (head == nullptr)
+	{
+		head = new Node<T>(data);
+	}
+	else
+	{
+
+	}
+}
+
+int main()
+{
+	setlocale(LC_ALL, "ru");
+	List<int> lst;
+	lst.push_back(5);
+	return 0;
+
 }
